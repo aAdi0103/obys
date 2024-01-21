8d64c1e997c5324ee88afec89db8be63edae32bd
aAdi0103 <ak0755591@gmail.com>
2024-01-21 10:49:34 +0530

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-gsap.from(".load h1",{
+var tl = gsap.timeline()
+
+tl.from(".load h1,.load h2",{
     y:120,
+    duration:0.7,
+    delay:0.2,
+    stagger:0.2
+})
+tl.to(".load",{
     opacity:0,
-    duration:0.9,
-    delay:0.4,
-    stagger:0.4
+    delay:2.8,
+    duration:1,
+    stagger:-0.2
+})
+tl.to("#loader",{
+    top:"-100%",
+    duration:2,
+    ease:"power4.out"
 })
 
 var time=document.querySelector(".timer h3")
@@ -14,3 +26,6 @@ setInterval(function(){
      }
      time.innerHTML=grow
 },50)
+setTimeout(function(){
+    clearInterval(int)
+},3000)
